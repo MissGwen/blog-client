5e47c2ca705a04a360334786ba667aff5b52e1d1
Zoe <1526566980@qq.com>
2024-03-29 22:24:28 +0800

    feat(globals): Add lint-staged.config.mjs

--- lint-staged.config.mjs
+++ lint-staged.config.mjs
@@ -3,6 +3,9 @@ import path from 'path'
 const buildEslintCommand = filenames =>
   `next lint --fix --file ${filenames.map(f => path.relative(process.cwd(), f)).join(' --file ')}`
 
-module.exports = {
-  '*.{js,jsx,ts,tsx}': [buildEslintCommand]
+const config = {
+  '*.{js,jsx,ts,tsx}': [buildEslintCommand],
+  '*': 'prettier --write .'
 }
+
+export default config
